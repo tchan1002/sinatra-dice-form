df8e8ec59f93f41d19c39f68341aa3bb280a8a8a
tchan1002 <tchan0@uchicago.edu>
2024-10-29 20:43:48 +0000

    triumph

--- app.rb
+++ app.rb
@@ -2,8 +2,18 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+ erb(:form)
+end
+
+get("/process_roll") do
+  @dice = params.fetch("dice").to_i
+  @sides = params.fetch("sides").to_i
+  @rolls = []
+
+  @dice.times do
+    die = rand(1..@sides)
+
+    @rolls.push(die)
+  end
+  erb(:results)
 end
